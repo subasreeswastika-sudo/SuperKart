b999df948bd6b1c263058cb503a9ec20a28e4880
subasreeswastika-sudo <subasree.swastika@gmail.com>
2026-04-27 11:08:25 +0530

    Update app.py

--- deployment/app.py
+++ deployment/app.py
@@ -4,7 +4,7 @@ from huggingface_hub import hf_hub_download
 import joblib
 
 # Download and load the model
-model_path = hf_hub_download(repo_id="swastisubi/SuperKart", filename="SuperKart_model_v1.joblib")
+model_path = hf_hub_download(repo_id="swastisubi/SuperKart", filename="model.joblib")
 model = joblib.load(model_path)
 
 # Streamlit UI for SuperKart Sales Prediction
