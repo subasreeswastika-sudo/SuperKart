7ff77aff71406d42a1822d95a76cd7333b6c91d2
subasreeswastika-sudo <subasree.swastika@gmail.com>
2026-04-27 23:44:25 +0530

    Update hosting.py

--- hosting/hosting.py
+++ hosting/hosting.py
@@ -1,10 +1,12 @@
 from huggingface_hub import HfApi
 import os
-
+#Initialize API with token from GitHub Secrets
 api = HfApi(token=os.getenv("HF_TOKEN"))
+repo_id="swastisubi/SuperKart"
 api.upload_folder(
-    folder_path="SuperKart/deployment",     # the local folder containing your files
-    repo_id="swastisubi/SuperKart",          # the target repo
+    folder_path=".",     # the local folder containing your files
+    repo_id=repo_id,          # the target repo
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
 )
+print(f"Deployement to {repo_id} successful!")
